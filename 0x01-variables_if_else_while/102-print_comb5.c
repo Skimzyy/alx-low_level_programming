91d6f965de0fb44be17ca4f21e2bed6064e8c1b9
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-09 19:12:13 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while(n1 <= 99)
+	{
+		n2 = n1;
+		while(n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (n1 !+ 98 || n2 != 99)
+				{
+					putchar(',');
+					putvhar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}

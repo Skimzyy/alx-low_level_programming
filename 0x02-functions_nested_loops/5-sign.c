ddae76b0e90f1f51ec35cfd0b4000b62fe424ab2
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-13 17:36:17 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sing - function to check for a sign of a number
- * @n: is the int that we use for the argument of the function
+ * @n: is the int that will use for the argument of the function
  * Return: Always 0 (Success)
  */
 int print_sign(int n)

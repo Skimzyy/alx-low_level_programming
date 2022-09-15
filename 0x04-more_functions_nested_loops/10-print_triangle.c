00894e8b18449c9b3283ad399099da3bec57b584
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-15 16:36:15 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((inc2 = size -inc1); inc2 > 0; inc2--)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
@@ -33,4 +33,4 @@ void print_triangle(int size)
 		}
 	}
 	putchar('\n');
-}		
+}

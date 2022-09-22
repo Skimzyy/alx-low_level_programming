4004381ea1734385b6dc0023c164594d8c0055fd
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-22 15:01:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,24 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i,j,k;
-	int len1;
-	int len2;
+	int index = 0, dest_len = 0;
 
-	len1 = 0;
-	len2 = 0;
-	for (i = 0; dest[i] != '\0'; ++i)
-	{
-		len1++;
-	}
-	for (j = 0; src[j] != '\0'; ++j)
-	{
-		len2++;
-	}
-	for (k = 0; k < len2; ++k)
-	{
-		dest[len1 + k] = scr[k];
-	}
-	dest[len1 + k] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; scr[index]; index++)
+		dest[dest_len++] = scr[index];
+
 	return (dest);
 }

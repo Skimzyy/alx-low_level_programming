1a83b36108381c97c83b026059790abd302f9704
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-23 06:26:15 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates strings
+ * @dest: destination
+ * @src: source
+ * Return: char *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+	char *ptr;
+
+	for (j = 0; dest[j] != '\0'; ++j)
+	{
+		len++;
+	}
+	*ptr = dest + len;
+	for (i = 0; src[i] != '\0' && i < n; ++i)
+	{
+		*ptr++ = src[i];
+	}
+	*ptr = '\0';
+}

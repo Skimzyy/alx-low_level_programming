3a509e2d7a96dda6fd37b4e6aead1de5a42e7224
Skimzyy <Ogundimuroyan@yahoo.com>
2022-09-28 04:37:57 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,11 +5,13 @@
  * @s: pointer to the string
  * Return: void
  */
-void _print_rev_recursion(char *s);
+
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
+
 }
